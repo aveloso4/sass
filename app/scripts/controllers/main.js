d6d79a1b2b9dcf6549c3a7c418819ce6dbf203e5
agustin veloso <agustin.veloso@globant.com>
2014-11-05 14:05:34 -0300

    SASS and HTML corrections. Options repeater implemented

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,24 @@ angular.module('sassApp')
       'AngularJS',
       'Karma'
     ];
+
+	$scope.items = [
+		{
+			name: '3-Prong 5\' Flat Dryer Cord - Gray',
+			price: 29.99,
+			selected: true
+		},
+		{
+			name: '4-Prong 5\' Round Dryer Cord - Black',
+			price: 29.99
+		},
+		{
+			name: '6 ft. Snap-Lock&amp;trade; Flexible Pipe',
+			price: 14.99
+		},
+		{
+			name: 'Dryer Exhaust Close Elbow',
+			price: 9.99
+		}
+	]
   });
